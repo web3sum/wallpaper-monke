f43f9f36296a34306b8d37b3881dc1dc740dcc42
wallpaper monke <slutcointrillionaire@gmail.com>
2022-08-04 18:26:14 +1200

    yo

--- commands/image.js
+++ commands/image.js
@@ -49,7 +49,7 @@ module.exports = {
     const ctx = canvas.getContext('2d');
 
     const options = {
-      url: 'https://i.imgur.com/uuprz9M.png',
+      url: 'https://github.com/web3sum/wallpaper-monke/blob/main/1170/1.png?raw=true',
       method: 'get',
       encoding: null,
     };
