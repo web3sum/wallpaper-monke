c6cde4be762e495398e9fddb06af3ff3602755c4
wallpaper monke <slutcointrillionaire@gmail.com>
2022-08-04 17:49:10 +1200

    yo

--- commands/image.js
+++ commands/image.js
@@ -57,7 +57,7 @@ module.exports = {
     console.log(blob);
     const monke = await Canvas.loadImage(
       // 'https://drive.google.com/file/d/156YX_aC0duPJ9Zb7JYMEDgD34o1PcXHB/view?usp=sharing'
-      URL.createObjectURL({ obj: blob })
+      { obj: URL.createObjectURL(blob) }
     );
 
     // path.join(__dirname, '..', '1170', `${id}.png`)
