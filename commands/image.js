8264f902de26bef8a5e1fdf289a2c37daf232d3c
wallpaper monke <slutcointrillionaire@gmail.com>
2022-08-04 13:18:18 +1200

    first commit

---
+++ commands/image.js
@@ -0,0 +1,65 @@
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
+const Canvas = require('@napi-rs/canvas');
+const path = require('node:path');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('monke')
+    .setDescription('wallpaper for your monke!')
+    .addIntegerOption((option) =>
+      option
+        .setName('id')
+        .setDescription('the id of your monke!')
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName('color')
+        .setDescription('color of the background')
+        .setRequired(true)
+        .addChoices(
+          { name: 'yellow', value: 'yellow' },
+          { name: 'orange', value: 'orange' },
+          { name: 'pink', value: 'pink' },
+          { name: 'purple', value: 'purple' },
+          { name: 'blue', value: 'blue' },
+          { name: 'green', value: 'green' }
+        )
+    ),
+  async execute(interaction) {
+    const id = interaction.options.getInteger('id');
+    const user = interaction.user;
+    // console.log(id);
+    if (id < 1 || id > 5000) {
+      await interaction.reply(
+        `hey <@${user.id}>, id must be between 1 and 5000!`
+      );
+      return;
+    }
+    await interaction.reply(
+      `hey <@${user.id}>, hang tight! monke #${id} will swing around soon!`
+    );
+
+    const canvas = Canvas.createCanvas(1170, 2532);
+    const ctx = canvas.getContext('2d');
+    const monke = await Canvas.loadImage(
+      path.join(__dirname, '..', '1170', `${id}.png`)
+      // `../img/${interaction.options.getString('id')}.png`
+    );
+    const color = interaction.options.getString('color');
+    const background = await Canvas.loadImage(
+      path.join(__dirname, '..', 'phone', `ip12-${color}.png`)
+    );
+    ctx.drawImage(background, 0, 0);
+    // ctx.imageSmoothingEnabled = false;
+    // ctx.imageSmoothingEnabled = false;
+
+    // ctx.imageSmoothingQuality = 'low';
+    ctx.drawImage(monke, 0, 1362);
+
+    const attachment = new AttachmentBuilder(await canvas.encode('png'), {
+      name: `${id}.png`,
+    });
+    await interaction.followUp({ files: [attachment] });
+  },
+};
