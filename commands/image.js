efbe30f4f9f3fb26f797c5526d386b7e828ed5dc
wallpaper monke <slutcointrillionaire@gmail.com>
2022-08-04 17:25:29 +1200

    yo

--- commands/image.js
+++ commands/image.js
@@ -3,6 +3,7 @@ const Canvas = require('@napi-rs/canvas');
 const path = require('node:path');
 const https = require('https');
 const { URL } = require('node:url');
+const fetch = require('node-fetch');
 
 module.exports = {
   data: new SlashCommandBuilder()
